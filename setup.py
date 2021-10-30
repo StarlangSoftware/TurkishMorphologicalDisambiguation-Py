10e7644d134f01eb4ea35e470ecbba42078d838d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:58:26 +0300

    Updated setup.py and dictionary.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-MorphologicalDisambiguation',
-    version='1.0.12',
+    version='1.0.13',
     packages=['MorphologicalDisambiguation'],
     url='https://github.com/StarlangSoftware/TurkishMorphologicalDisambiguation-Py',
     license='',
-    author='olcay',
+    author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Morphological Disambiguation Library',
-    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram']
+    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-NGram'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
