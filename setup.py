366931da2718cbeb9f22de17fc763bb9e7304e0b
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-17 23:35:20 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-MorphologicalDisambiguation',
-    version='1.0.14',
+    version='1.0.15',
     packages=['MorphologicalDisambiguation', 'MorphologicalDisambiguation.data'],
     package_data={'MorphologicalDisambiguation.data': ['*.txt']},
     url='https://github.com/StarlangSoftware/TurkishMorphologicalDisambiguation-Py',
